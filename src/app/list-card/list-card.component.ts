04066b6ace75321f1da31d8d8632acb55b9063b4
vdt2210 <vdt1999@outlook.com>
2021-12-09 00:54:54 +0700

    fix infinite scroll

--- src/app/list-card/list-card.component.ts
+++ src/app/list-card/list-card.component.ts
@@ -11,97 +11,114 @@ export class ListCardComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.showVehicle();
+  }
 
   public detailicon: string = 'chevron-forward';
 
-  public data: any = [
-    {
-      id: 1,
-      brand: 'Honda',
-      name: 'NSX',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Honda-NSX.png',
-    },
-    {
-      id: 2,
-      brand: 'Suzuki',
-      name: 'XL7',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://saigonngoisao.com.vn/vnt_upload/product/color/magma_grey_zeta.png',
-    },
-    {
-      id: 3,
-      brand: 'BWM',
-      name: '3 Series',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://www.bmw.vn/content/dam/bmw/common/all-models/3-series/sedan/2018/navigation/bmw-3-series-modellfinder.png',
-    },
-    {
-      id: 4,
-      brand: 'Mercedes-Benz',
-      name: 'c180',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://i.xeoto.com.vn/auto/mercedes/c180/mercedes-c180-phien-ban-16-turbo.png',
-    },
-    {
-      id: 5,
-      brand: 'Ford',
-      name: 'Raptor',
-      year: 2019,
-      desc: 'New car',
-      img: 'https://www.ford.com.vn/content/dam/Ford/website-assets/ap/vn/nameplate/raptor-2019/thumbnails/raptor.jpg',
-    },
-    {
-      id: 6,
-      brand: 'Toyota',
-      name: 'Supra',
-      year: 1998,
-      desc: 'New car',
-      img: 'http://data.3dtuning.com/tun/6SOdFYFSRH.jpg',
-    },
-    {
-      id: 7,
-      brand: 'Subaru',
-      name: 'WRX STI',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://www.subaru.asia/assets/content/vehicles/media/thumbnails/vn_my18_wrxsti_25_6mt.png',
-    },
-    {
-      id: 8,
-      brand: 'Misubishi',
-      name: 'Triton Athlete',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://www.mitsubishi-motors.com.vn/w/wp-content/uploads/2021/11/SU-SAM1.png',
-    },
-    {
-      id: 9,
-      brand: 'Bugatti',
-      name: 'Chiron',
-      year: 'N/A',
-      desc: 'New car',
-      img: 'https://img.tinbanxe.vn/webp/images/Bugatti/Bugatti%20Chiron/bugatti-chiron-gallery-01-removebg-preview.png',
-    },
-    {
-      id: 10,
-      brand: 'Lamborghini',
-      name: 'Huracan',
-      year: 2020,
-      desc: 'New car',
-      img: 'https://i.xeoto.com.vn/auto/lamborghini/huracan/lamborghini-huracan-2020-61144.png',
-    },
-  ];
+  public data = [];
+  private moreVehicle = [];
+
+  showVehicle() {
+    this.data = [
+      {
+        id: 1,
+        brand: 'Honda',
+        name: 'NSX',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Honda-NSX.png',
+      },
+      {
+        id: 2,
+        brand: 'Suzuki',
+        name: 'XL7',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://saigonngoisao.com.vn/vnt_upload/product/color/magma_grey_zeta.png',
+      },
+      {
+        id: 3,
+        brand: 'BWM',
+        name: '3 Series',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://www.bmw.vn/content/dam/bmw/common/all-models/3-series/sedan/2018/navigation/bmw-3-series-modellfinder.png',
+      },
+      {
+        id: 4,
+        brand: 'Mercedes-Benz',
+        name: 'c180',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://i.xeoto.com.vn/auto/mercedes/c180/mercedes-c180-phien-ban-16-turbo.png',
+      },
+    ];
+    console.log(this.data);
+  }
+
+  showMoreVehicle() {
+    this.moreVehicle = [
+      {
+        id: 5,
+        brand: 'Ford',
+        name: 'Raptor',
+        year: 2019,
+        desc: 'New car',
+        img: 'https://www.ford.com.vn/content/dam/Ford/website-assets/ap/vn/nameplate/raptor-2019/thumbnails/raptor.jpg',
+      },
+      {
+        id: 6,
+        brand: 'Toyota',
+        name: 'Supra',
+        year: 1998,
+        desc: 'New car',
+        img: 'http://data.3dtuning.com/tun/6SOdFYFSRH.jpg',
+      },
+      {
+        id: 7,
+        brand: 'Subaru',
+        name: 'WRX STI',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://www.subaru.asia/assets/content/vehicles/media/thumbnails/vn_my18_wrxsti_25_6mt.png',
+      },
+      {
+        id: 8,
+        brand: 'Misubishi',
+        name: 'Triton Athlete',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://www.mitsubishi-motors.com.vn/w/wp-content/uploads/2021/11/SU-SAM1.png',
+      },
+      {
+        id: 9,
+        brand: 'Bugatti',
+        name: 'Chiron',
+        year: 'N/A',
+        desc: 'New car',
+        img: 'https://img.tinbanxe.vn/webp/images/Bugatti/Bugatti%20Chiron/bugatti-chiron-gallery-01-removebg-preview.png',
+      },
+      {
+        id: 10,
+        brand: 'Lamborghini',
+        name: 'Huracan',
+        year: 2020,
+        desc: 'New car',
+        img: 'https://i.xeoto.com.vn/auto/lamborghini/huracan/lamborghini-huracan-2020-61144.png',
+      },
+    ];
+    this.moreVehicle.forEach((e) => {
+      this.data.push(e);
+    });
+    console.log(this.data);
+  }
 
   loadData(event) {
     setTimeout(() => {
       event.target.complete();
-
+      this.showMoreVehicle();
       if (this.data.length == this.data.length) {
         event.target.disabled = true;
       }
