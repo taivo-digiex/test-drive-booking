5c224de8074a7ff429dc26f436775dfebf22ac4b
vdt2210 <vdt1999@outlook.com>
2021-12-16 00:48:31 +0700

    Update tabs-routing.module.ts

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -12,11 +12,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('../home/home.module').then((m) => m.HomePageModule),
       },
-      // {
-      //   path: 'tab2',
-      //   loadChildren: () =>
-      //     import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
-      // },
+      {
+        path: 'tab2',
+        loadChildren: () =>
+          import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
+      },
       {
         path: 'about',
         loadChildren: () =>
